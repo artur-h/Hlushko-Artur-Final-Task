4bf3fe083cbf4de4310c0d4a258509659c062563
sergey.tarasov <sergey.tarasov@technorely.com>
2020-02-29 23:15:40 +0200

    Added tablet filter menu

--- js/main.js
+++ js/main.js
@@ -61,43 +61,67 @@ function closeMobileMenu() {
 }
 
 
-let tabMenu = document.getElementById('tab-menu');
-tabMenu.addEventListener('click', function(e) {
+// let tabMenu = document.getElementById('tab-menu');
+// tabMenu.addEventListener('click', function(e) {
+//   let target = e.target;
+//
+//   if (target.className === 'filter__menu-item') {
+//     let data = target.dataset.type;
+//     let el = document.querySelectorAll('*');
+//     let elems = [];
+//
+//     for (let i = 0; i < el.length; i++) {
+//       if (el[i].dataset.type === data && el[i].className !== 'filter__menu-item') {
+//         elems.push(el[i]);
+//       }
+//     }
+//
+//     let item;
+//     let result;
+//     let name;
+//     let choice;
+//
+//     for (let i = 0; i < elems.length; i++) {
+//       if (elems[i].className === 'filter__result' || elems[i].className === 'filter__result filter__result--selected') result = elems[i];
+//       if (elems[i].className === 'filter__name' || elems[i].className === 'filter__name filter__name--selected') name = elems[i];
+//       if (elems[i].className === 'filter__choice' || elems[i].className === 'filter__choice filter__choice--selected') choice = elems[i];
+//       if (elems[i].className === 'filter__item' || elems[i].className === 'filter__item filter__item--selected') item = elems[i];
+//     }
+//
+//     if (target.textContent !== 'Not selected') {
+//       item.className = 'filter__item filter__item--selected';
+//       result.className = 'filter__result filter__result--selected';
+//       name.className = 'filter__name filter__name--selected';
+//       choice.textContent = target.textContent;
+//     } else if (target.textContent === 'Not selected') {
+//       item.className = 'filter__item';
+//       result.className = 'filter__result';
+//       name.className = 'filter__name';
+//       choice.textContent = '';
+//     }
+//   }
+// });
+
+let tabletTabMenu = document.getElementById('tablet-tab-menu');
+tabletTabMenu.addEventListener('click', function(e) {
   let target = e.target;
+  let menu = document.getElementById('tablet-menu');
+  let closeIcon = document.getElementById('close-filter-menu');
+  let showIcon = document.getElementById('show-filter-menu');
 
-  if (target.className === 'filter__menu-item') {
-    let data = target.dataset.type;
-    let el = document.querySelectorAll('*');
-    let elems = [];
+  if (target.className === 'tablet-filter__inner' ||
+      target.className === 'tablet-filter__picked' ||
+      target.className === 'tablet-filter__arrow-icon' ||
+      target.className === 'tablet-filter__wrapper') {
 
-    for (let i = 0; i < el.length; i++) {
-      if (el[i].dataset.type === data && el[i].className !== 'filter__menu-item') {
-        elems.push(el[i]);
-      }
+    if (menu.className === 'tablet-filter__menu display-none') {
+      closeIcon.className = 'tablet-filter__close-icon';
+      showIcon.className = 'tablet-filter__arrow-icon display-none';
+      menu.className = 'tablet-filter__menu';
     }
-
-    let item;
-    let result;
-    let name;
-    let choice;
-
-    for (let i = 0; i < elems.length; i++) {
-      if (elems[i].className === 'filter__result' || elems[i].className === 'filter__result filter__result--selected') result = elems[i];
-      if (elems[i].className === 'filter__name' || elems[i].className === 'filter__name filter__name--selected') name = elems[i];
-      if (elems[i].className === 'filter__choice' || elems[i].className === 'filter__choice filter__choice--selected') choice = elems[i];
-      if (elems[i].className === 'filter__item' || elems[i].className === 'filter__item filter__item--selected') item = elems[i];
-    }
-
-    if (target.textContent !== 'Not selected') {
-      item.className = 'filter__item filter__item--selected';
-      result.className = 'filter__result filter__result--selected';
-      name.className = 'filter__name filter__name--selected';
-      choice.textContent = target.textContent;
-    } else if (target.textContent === 'Not selected') {
-      item.className = 'filter__item';
-      result.className = 'filter__result';
-      name.className = 'filter__name';
-      choice.textContent = '';
-    }
+  } else if (target.className === 'tablet-filter__close-icon') {
+    menu.className = 'tablet-filter__menu display-none';
+    showIcon.className = 'tablet-filter__arrow-icon';
+    closeIcon.className = 'tablet-filter__close-icon display-none';
   }
 });
