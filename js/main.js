711b61a9da7e53952021a77236f853d81044f04c
artur-h <artur.hlushko@gmail.com>
2020-03-01 13:04:20 +0200

    Finished tablet tab menu

--- js/main.js
+++ js/main.js
@@ -101,6 +101,14 @@ function closeMobileMenu() {
 //     }
 //   }
 // });
+let selectedTabletFilterElements = {
+  'fashion': [],
+  'productType': [],
+  'color': [],
+  'brand': [],
+  'size': [],
+  'priceRange': []
+};
 
 let tabletTabMenu = document.getElementById('tablet-tab-menu');
 tabletTabMenu.addEventListener('click', function(e) {
@@ -110,7 +118,7 @@ tabletTabMenu.addEventListener('click', function(e) {
   let showIcon = document.getElementById('show-filter-menu');
 
   if (target.className === 'tablet-filter__inner' ||
-      target.className === 'tablet-filter__picked' ||
+      target.className === 'tablet-filter__options' ||
       target.className === 'tablet-filter__arrow-icon' ||
       target.className === 'tablet-filter__wrapper') {
 
@@ -124,4 +132,54 @@ tabletTabMenu.addEventListener('click', function(e) {
     showIcon.className = 'tablet-filter__arrow-icon';
     closeIcon.className = 'tablet-filter__close-icon display-none';
   }
+
+  if (target.className === 'tablet-filter__list-item') {
+    target.className = 'tablet-filter__list-item tablet-filter__list-item--selected';
+
+    let data = target.dataset.type;
+    let el = document.querySelectorAll('*');
+    let elems = [];
+
+    for (let key in selectedTabletFilterElements) {
+      if (key === data) {
+        selectedTabletFilterElements[key].push(target);
+
+        if (selectedTabletFilterElements[key].length === 2) {
+          selectedTabletFilterElements[key][0].className = 'tablet-filter__list-item';
+          selectedTabletFilterElements[key][1].className = 'tablet-filter__list-item tablet-filter__list-item--selected';
+          selectedTabletFilterElements[key].shift();
+        }
+      }
+    }
+
+    for (let i = 0; i < el.length; i++) {
+      if (el[i].dataset.type === data && el[i].className !== 'tablet-filter__list-item') {
+        elems.push(el[i]);
+      }
+    }
+
+    let result;
+    let sectionName;
+    let notSelected;
+
+    for (let i = 0; i < elems.length; i++) {
+      if (elems[i].className === 'tablet-filter__options' || elems[i].className === 'tablet-filter__options tablet-filter__options--selected') result = elems[i];
+      if (elems[i].className === 'tablet-filter__heading') sectionName = elems[i];
+      if (elems[i].className === 'tablet-filter__list-item tablet-filter__list-item--not-selected' || elems[i].className === 'tablet-filter__list-item') notSelected = elems[i];
+    }
+
+    if (target.textContent !== 'Not selected') {
+      result.className = 'tablet-filter__options tablet-filter__options--selected';
+      result.textContent = target.textContent;
+
+      if (notSelected !== undefined) {
+        notSelected.className = 'tablet-filter__list-item';
+      }
+    } else if (target.textContent === 'Not selected') {
+      target.className = 'tablet-filter__list-item tablet-filter__list-item--not-selected';
+      result.className = 'tablet-filter__options';
+      result.textContent = sectionName.textContent;
+    }
+  }
+  console.log(selectedTabletFilterElements);
 });
